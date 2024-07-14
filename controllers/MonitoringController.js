5c191d05e692f71bc59fb78b19f9f6faa06cc0fd
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-07-14 20:59:30 +0700

    push

--- controllers/MonitoringController.js
+++ controllers/MonitoringController.js
@@ -1,58 +1,59 @@
 const Models = require("../models/index.js");
-const {
-  handlerError,
-  handleGet,
-} = require("../helper/HandlerError.js");
+
 class MonitoringController {
   static async getMonitoring(req, res) {
     try {
-      // const ListDevice = await Models.ListDevice.findAll()
-      // const ListSensor = await Models.ListSensor.findAll()
       const sensorData = await Models.DetailSensor.findAll();
       // const DataValues = await Models.DataValues.findAll()
-      
-      // return res.send(sensorData);
+
+      // return res.send(DetailSensor);
 
       // Menginisialisasi objek untuk menyimpan data yang sudah dikelompokkan berdasarkan device
-const deviceData = {};
+      const deviceData = {};
 
-// Loop melalui data sensor untuk mengelompokkan dan menghitung data yang diperlukan
-sensorData.forEach(sensor => {
-  const deviceName = sensor.sensordata.parentdevicename;
-  
-  // Jika device belum ada di dalam objek, tambahkan
-  if (!deviceData[deviceName]) {
-    deviceData[deviceName] = {
-      device: deviceName,
-      maxPingTime: 0,
-      maxPingJitterTime: 0,
-      snmpDateTime: '',
-      snmpUptimeStrength: ''
-    };
-  }
+      // Loop melalui data sensor untuk mengelompokkan dan menghitung data yang diperlukan
+      sensorData.forEach((sensor) => {
+        const deviceName = sensor.sensordata.parentdevicename;
 
-  // Proses data berdasarkan nama sensor
-  if (sensor.sensordata.name === 'Ping') {
-    const pingTime = parseFloat(sensor.sensordata.lastvalue.split(' ')[0]);
-    if (pingTime > deviceData[deviceName].maxPingTime) {
-      deviceData[deviceName].maxPingTime = pingTime;
-    }
-  } else if (sensor.sensordata.name === 'Ping Jitter') {
-    const pingJitterTime = parseFloat(sensor.sensordata.lastvalue.split(' ')[0]);
-    if (pingJitterTime > deviceData[deviceName].maxPingJitterTime) {
-      deviceData[deviceName].maxPingJitterTime = pingJitterTime;
-    }
-  } else if (sensor.sensordata.name === 'SNMP System Uptime') {
-    deviceData[deviceName].snmpDateTime = sensor.sensordata.lastup.split(' ')[0];
-    deviceData[deviceName].snmpUptimeStrength = parseFloat(sensor.sensordata.uptime.replace('%', ''));
-  }
-});
+        // Jika device belum ada di dalam objek, tambahkan
+        if (!deviceData[deviceName]) {
+          deviceData[deviceName] = {
+            device: deviceName,
+            maxPingTime: 0,
+            maxPingJitterTime: 0,
+            snmpDateTime: "",
+            snmpUptimeStrength: "",
+          };
+        }
 
-// Mengubah objek menjadi array
-const result = Object.values(deviceData);
+        // Proses data berdasarkan nama sensor
+        if (sensor.sensordata.name === "Ping") {
+          const pingTime = parseFloat(
+            sensor.sensordata.lastvalue.split(" ")[0]
+          );
+          if (pingTime > deviceData[deviceName].maxPingTime) {
+            deviceData[deviceName].maxPingTime = pingTime;
+          }
+        } else if (sensor.sensordata.name === "Ping Jitter") {
+          const pingJitterTime = parseFloat(
+            sensor.sensordata.lastvalue.split(" ")[0]
+          );
+          if (pingJitterTime > deviceData[deviceName].maxPingJitterTime) {
+            deviceData[deviceName].maxPingJitterTime = pingJitterTime;
+          }
+        } else if (sensor.sensordata.name === "SNMP System Uptime") {
+          deviceData[deviceName].snmpDateTime =
+            sensor.sensordata.lastup.split(" ")[0];
+          deviceData[deviceName].snmpUptimeStrength = parseFloat(
+            sensor.sensordata.uptime.replace("%", "")
+          );
+        }
+      });
 
-console.log(result);
+      // Mengubah objek menjadi array
+      const result = Object.values(deviceData);
 
+      console.log(result);
     } catch (error) {
       handlerError(res, error);
     }
