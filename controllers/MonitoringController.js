8be49054ae2fbd0404c3fda4e340b44ff1ec56f2
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-07-14 19:48:35 +0700

    handel again

--- controllers/MonitoringController.js
+++ controllers/MonitoringController.js
@@ -1,86 +1,15 @@
 const Models = require("../models/index.js");
-const { handleGet, handlerError } = require("../helper/HandlerError");
-const connectionPRTG = require("../utils/connection");
-
-async function fetchDevices() {
-  const data = await connectionPRTG.tableJson(
-    "content=devices&usecaption=true&filter_parentid=2067&columns=objid,device"
-  );
-  console.log(data)
-  // const response = await fetch(url);
-  // const data = await response.json();
-  return data.devices;
-}
-
-async function fetchSensors(deviceId) {
-  const data =
-    await connectionPRTG.tableJson(`content=sensors&usecaption=true&filter_parentid=${deviceId}&columns=objid,sensor`);
-  // const response = await fetch(url);
-  // const data = await response.json();
-  return data.sensors;
-}
-
-async function fetchSensorDetails(sensorId) {
-  const data = await connectionPRTG.detailSensor(`id=${sensorId}&usecaption=true`);
-  // const response = await fetch(url);
-  // const data = await response.json();
-  return data.sensordata;
-}
-
 
 class MonitoringController {
   static async getMonitoring(req, res) {
     try {
-      const ListDevice = await Models.ListDevice.findAll({attributes: ['devices']})
+      const ListDevice = await Models.ListDevice.findAll()
       const ListSensor = await Models.ListSensor.findAll()
       const DetailSensor = await Models.DetailSensor.findAll()
       const DataValues = await Models.DataValues.findAll()
       
-      // const monitoring = ListDevice[0].devices.map((data, x)=>{
-      //   const{ objid, device} = data
-      //   console.log(device)
-      //   return{
-      //     objid,
-      //     device
-      //   }
-      // })
-  
-      return res.send(ListSensor)
-      // return res.send(ListDevice[0].devices)
+      return res.send(ListDevice)
 
-    } catch (error) {
-      handlerError(res, error);
-    }
-  }
-  static async getMonitoringLive(req, res) {
-    try {
-      // const ListDevice = await connectionPRTG.tableJson(
-      //   "content=devices}&usecaption=true&filter_parentid=${groupID}&columns=objid,device"
-      // );
-      // const ListSensor = await connectionPRTG.tableJson(
-      //   "content=sensors}&usecaption=true&filter_parentid=${deviceId}&columns=objid,sensor"
-      // );
-      // const DetailSensor = await connectionPRTG.detailSensor(
-      //   "id=${sensorId}&usecaption=true"
-      // );
-
-      const devices = await fetchDevices();
-      const allDetails = [];
-
-      for (const device of devices) {
-        const sensors = await fetchSensors(device.objid);
-
-        for (const sensor of sensors) {
-          const details = await fetchSensorDetails(sensor.objid);
-          allDetails.push({
-            deviceName: device.device,
-            sensorName: sensor.sensor,
-            details: details,
-          });
-        }
-      }
-      console.log(allDetails);
-      return res.send(allDetails)
     } catch (error) {
       handlerError(res, error);
     }
